ec6829efae37214d8c9225574641eabe5932ad7e
coskun cakir <csknckr@gmail.com>
2020-06-21 19:06:00 +0100

    memoji added

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/assets/img/memoji-co.png`, // This path is relative to the root of the site.
+        icon: `src/assets/img/memoji.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
